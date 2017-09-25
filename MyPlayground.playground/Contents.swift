cde23c0a8f4d5b3e4bba396062baf047bf03cede
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-09-25 08:37:54 -0600

    Deleted playground repo.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+// explicit type declaration with no value
+var ramen :String
+// mplicit type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge + 382402
+var someValue = 3.1415 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Cade"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
+var height = 6
+
